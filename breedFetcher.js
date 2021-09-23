edbac5f73cd9bebeb42c8d4ae8c2a9340c45397d
jvblum <jvmell.skch@gmail.com>
2021-09-23 17:52:23 -0400

    linting

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,13 @@
 const request = require('request');
 
 const fetchBreedDescription = (breedName, callback) => {
-  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`; // cannot handle 404 message on api // outputs {message: 404 ...}; 
-    request(url, (error, response, body) => {
-      if (error) return console.log(error);
-      const data = JSON.parse(body);
-      if (data.length === 0) return callback(null, "no cats here 🐁");
-      if (data[0]) if(data[0].description) callback(null, data[0].description); // the checks are for the {message:...} mentioned above;
-    });
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`; // cannot handle 404 message on api // outputs {message: 404 ...};
+  request(url, (error, response, body) => {
+    if (error) return console.log(error);
+    const data = JSON.parse(body);
+    if (data.length === 0) return callback(null, "no cats here 🐁");
+    if (data[0]) if (data[0].description) callback(null, data[0].description); // the checks are for the {message:...} mentioned above;
+  });
 };
 
 module.exports = fetchBreedDescription;
